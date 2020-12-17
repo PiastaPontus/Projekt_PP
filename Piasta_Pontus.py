c90fa5ffa9c0e3613b4b5d81963e9694c09f62c7
PiastaPontus <piastapontus@gmail.com>
2020-12-17 19:04:55 +0100

    1.1

--- Piasta_Pontus.py
+++ Piasta_Pontus.py
@@ -5,6 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 import math
+import wzory
 
 print('Oto program do symulacji ruchu poziomego z pewnej wysokośći H o danej prędkości początkowej V')
 
@@ -21,12 +22,20 @@ while True:
     except ValueError:
         print("Wprowadzono błędny typ danych, spróbuj jeszcze raz")
 
-plik = open('wzory.py','r')
 
-print('Dane: wysokość - h=', h, '[m]\n prędkość początkowa: v=', v, '[m/s]\n przyspieszenie ziemskie - g=', g,
-      '[m/s^2]')
+print('Dane:\n wysokość - h=', h, '[m]\n prędkość początkowa: v=', v, '[m/s]\n przyspieszenie ziemskie - g=', (wzory.g), '[m/s^2]\n\n')
 
-plik.close()
+print ('Czas lotu ciała wyniósł: ',(wzory.czas(h)),'[s]')
+print ('Zasięg lotu wyniósł: ',(wzory.zasieg(v,h)),'[m]')
+print ('Równanie toru ruchu ma postać: ',(wzory.rownanie(v,h)))
+
+
+
+
+
+
+
+
 
 #print(czas)
 #print(zasieg)
--- wzory.py
+++ wzory.py
@@ -1,6 +1,6 @@
-g = 9.81
-g = int(g)
+import math
 
-zasieg = v * math.sqrt(2*h/g)
-czas = math.sqrt(2*h/g)
-rownanie = ("y(x) = "+str(h)+" - ("+str(g)+"/2)/("+str(v)+"**2) * x^2")
+g = 9.81
+zasieg = lambda v, h: v * math.sqrt(2 * h / g)
+czas = lambda h: math.sqrt(2 * h / g)
+rownanie = lambda h, v: str("y(x) = " + str(h) + " - (" + str(g) + "/2)/(" + str(v) + "**2) * x^2")
