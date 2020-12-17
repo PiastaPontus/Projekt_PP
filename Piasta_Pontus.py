1b98bf2d9e0a9fa0d2dc5f674955c6ebcd72e63c
PiastaPontus <piastapontus@gmail.com>
2020-12-17 19:33:20 +0100

    1.1

--- Piasta_Pontus.py
+++ Piasta_Pontus.py
@@ -25,30 +25,19 @@ while True:
 
 print('Dane:\n wysokość - h=', h, '[m]\n prędkość początkowa: v=', v, '[m/s]\n przyspieszenie ziemskie - g=', (wzory.g), '[m/s^2]\n\n')
 
-print ('Czas lotu ciała wyniósł: ',(wzory.czas(h)),'[s]')
-print ('Zasięg lotu wyniósł: ',(wzory.zasieg(v,h)),'[m]')
+print('Wyniki:\n')
+print ('Czas lotu ciała wyniósł: ',round((wzory.czas(h)),3),'[s]')
+print ('Zasięg lotu wyniósł: ',round((wzory.zasieg(v,h)),3),'[m]')
 print ('Równanie toru ruchu ma postać: ',(wzory.rownanie(v,h)))
 
 
-
-
-
-
-
-
-
-#print(czas)
-#print(zasieg)
-#print(rownanie)
-
-# print('Wyniki') 17.12.2020r.
-
-# wykres (17.12.2020r.)
-# plt.plot(x,y,color='r', lw=1, ls='-', label='wzorek')
-# plt.legend()
-# plt.xlabel('X', fontsize = 8)
-# plt.ylabel('f(x)', fontsize = 8)
-# plt.show()
+x=np.arange(0,10)
+y= h - ((9,81 * (x**2)) / (2*(v**2)))
+plt.plot(x,y, color='r', lw=1, ls='-', label='wzorek')
+plt.legend()
+plt.xlabel('X', fontsize = 8)
+plt.ylabel('f(x)', fontsize = 8)
+plt.show()
 
 
 # Zapis do pliku (17.12.2020r.)
