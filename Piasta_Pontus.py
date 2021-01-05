e06547dc7c7f2b14166ebca4bba17fdae49e71fe
PiastaPontus <piastapontus@gmail.com>
2021-01-05 17:29:45 +0100

    1.1

--- Piasta_Pontus.py
+++ Piasta_Pontus.py
@@ -9,6 +9,7 @@ import wzory
 
 print('Oto program do symulacji ruchu poziomego z pewnej wysokośći H o danej prędkości początkowej V')
 
+#Pobieranie danych
 while True:
     try:
         h = float(input('Proszę podaj wysokość początkową ciała w metrach:'))
@@ -25,21 +26,32 @@ while True:
 
 print('Dane:\n wysokość - h=', h, '[m]\n prędkość początkowa: v=', v, '[m/s]\n przyspieszenie ziemskie - g=', (wzory.g), '[m/s^2]\n\n')
 
+#Wzory z pliku zew.
+czas = wzory.czas(h)
+zasieg = wzory.zasieg(v,h)
+rownanie = wzory.rownanie(v,h)
+
+#Obliczanie
 print('Wyniki:\n')
-print ('Czas lotu ciała wyniósł: ',round((wzory.czas(h)),3),'[s]')
-print ('Zasięg lotu wyniósł: ',round((wzory.zasieg(v,h)),3),'[m]')
+print ('Czas lotu ciała wyniósł: ',round(czas,3),'[s]')
+print ('Zasięg lotu wyniósł: ',round(zasieg,3),'[m]')
 print ('Równanie toru ruchu ma postać: ',(wzory.rownanie(v,h)))
 
+#Zapis do pliku
+f = open("wyniki.txt", "a")
+f.write('Oto program do symulacji ruchu poziomego z pewnej wysokośći H o danej prędkości początkowej V\n\n')
+f.write('Użytkownik podał anstępujące dane:\n * Wysokość początkowa: '+str(h)+'\n * Prędkość początkowa: '+str(v)+'\n\n')
+f.write('Otrzymane wyniki:\n')
+f.write(' * Równanie toru ruchu ma postać: '+ str(rownanie) + '\n * Zasięg lotu wyniósł: ' + str(zasieg) + '[m]\n * Czas lotu ciała wyniósł: ' + str(czas) + '[s]')
+f.close()
 
-x=np.arange(0,10) #JAkoś tutaj przeskalować
+#Wykres
+x=np.arange(0,int(zasieg + zasieg*0.05))
 y= h - ((9.81 * (x**2)) / (2*(v**2)))
 plt.plot(x,y, color='r', lw=1, ls='-', label='Tor ruchu ciała')
 plt.ylim(0)
 plt.legend()
 plt.xlabel('Odległość', fontsize = 8)
-plt.ylabel('f(x)', fontsize = 8)
+plt.ylabel('Wysokość', fontsize = 8)
 plt.savefig('wykres.jpg', dpi = 300)
 plt.show()
-
-
-#np.savetxt('wyniki.dat',a)
